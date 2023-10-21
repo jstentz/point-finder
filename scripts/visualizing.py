857420a72d74b36f93d626c0c3754734ac0a68d1
Jason Stentz <jstentz@andrew.cmu.edu>
2023-10-21 09:55:54 -0400

    broken ion

--- scripts/visualizing.py
+++ scripts/visualizing.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
+
+plt.ion()
 
 # Create a figure and a 3D axis
 screencolor = 'black'
@@ -13,28 +14,99 @@ ax.set_facecolor(screencolor)
 num_points = 500
 num_frames = 1000000
 
-# Generate random initial coordinates and colors for the points
-x = 2 * np.random.rand(num_points) - 1
-y = 2 * np.random.rand(num_points) - 1
-z = 2 * np.random.rand(num_points) - 1
-colors = np.random.rand(num_points, 3)
-sizes = 100000 * np.ones(num_points)
+points = 2 * np.random.rand(num_points, 3) - 1
+colors = np.zeros((num_points, 3))
+sizes = 100 * np.ones(num_points)
 
 # Initialize the scatter plot with colors
-scatter = ax.scatter(x, y, z, c=colors, s=sizes, marker='o', edgecolors=None)
+scatter = ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=colors, s=sizes, marker='o', edgecolors=None, alpha=0.4)
+
+
 
 # Define an update function for the animation to change colors
 def update(frame):
-  colors = np.random.rand(num_points, 3)  # Generate new random colors
+  global colors
+  step = np.pi / 200
+  t = step * frame
+
+  colorin = (np.sin(3 * (points[:, 0] + t)) / 1.3) < points[:, 2]
+
+  colors[colorin] = np.array([0, 0, 0])
+  colors[np.logical_not(colorin)] = np.array([1, 1, 1])
   scatter.set_color(colors)
+  # scatter.set_sizes(  )
 
-# Create the animation
-ani = FuncAnimation(fig, update, frames=num_frames, repeat=False)
 
-# Add a color bar to show the mapping of colors
-# cbar = fig.colorbar(scatter, ax=ax, label='Color')
+# t = 0
+# step = np.pi / 200
+# epsilon = 0.1
 
-# Show the animation
+# # Define an update function for the animation to change colors
+# def update(frame):
+#   global t, colors
+#   t += step
+
+#   colors[:, 0] = (np.sin(points[:, 0] + t) / 2) + 0.5
+#   colors[:, 1] = (np.sin(points[:, 1] + t + np.pi) / 2) + 0.5
+#   colors[:, 2] = (np.sin(points[:, 2] + t) / 2) + 0.5
+#   scatter.set_color(colors)
+
+
+# angle = 0
+# step = np.pi / 200
+# epsilon = 0.1
+
+# # Define an update function for the animation to change colors
+# def update(frame):
+#   global angle, colors
+#   angle += step
+
+#   # xsins = np.sin(points[:, 0] + angle)**2
+#   # colors = np.ones((num_points, 3)) * xsins
+#   colors = np.sin(points + angle)**2
+#   scatter.set_color(colors)
+
+
+
+# angle = 0
+# step = np.pi / 200
+# epsilon = 0.1
+
+# # Define an update function for the animation to change colors
+# def update(frame):
+#   global angle, colors
+#   colors *= 0.95
+#   angle += step
+#   plane1 = np.array([np.sin(angle), np.cos(angle), 0])
+#   plane2 = np.array([np.sin(angle - np.pi/2), np.cos(angle - np.pi/2), 0])
+
+#   # plane = np.array([0, 1, 0])
+#   # color1 = np.array([1, 0, 0])
+#   # color2 = np.array([0, 1, 0])
+#   green = np.array([1, 1, 1])
+#   red = np.array([1, 1, 1])
+#   # colors = np.random.rand(num_points, 3)  # Generate new random colors
+#   # colors = np.zeros((num_points, 3))
+#   for i in range(colors.shape[0]):
+#     if abs(np.dot(plane1, points[i])) < epsilon:
+#       colors[i] = green
+#     if abs(np.dot(plane2, points[i])) < epsilon:
+#       colors[i] = red
+#   scatter.set_color(colors)
+
+# # Create the animation
+ani = FuncAnimation(fig, update, frames=num_frames, interval=10, repeat=False)
+
+# # Show the animation
 plt.grid(False)
 plt.axis('off')
-plt.show()
+# plt.show()
+
+import time
+frame = 0
+# this doesn't update anything bruh
+while True:
+  update(frame)
+  plt.draw()
+  frame += 1
+  time.sleep(0.1)
